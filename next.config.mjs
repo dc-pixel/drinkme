b8da2c8fb8c616579dffff65d293a44798d1474c
DEV PRATAP SINGH <116972716+dc-pixel@users.noreply.github.com>
2026-09-15 23:37:43 +0530

    chore: configure Next.js image hosting

---
+++ next.config.mjs
@@ -0,0 +1,5 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: { remotePatterns: [{ protocol: 'images.unsplash.com', hostname: 'images.unsplash.com' }] }
+};
+export default nextConfig;
